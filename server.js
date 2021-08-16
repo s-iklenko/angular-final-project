ac031d43838a05a7a2a2864227027710a175cd82
serhii_iklenko <sergeyiklenko@gmail.com>
2021-08-16 13:26:30 +0300

    add config file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/disk/<angular-final-project>'));
+
+app.get('/', function (req, res){
+  res.sendFile(path.join(__dirname + '/disk/<angular-final-project>/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+

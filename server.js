5e04d5b1348f4e66b15d7ae2e03aa8c3193c192f
s.iklenko <sergeyiklenko@gmail.com>
2021-08-17 10:20:47 +0300

    added nav and game

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/disk/angular-final-project'));
 
 app.get('/', function (req, res){
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/disk/angular-final-project/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamesComponent } from "./games/games.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'games', component: GamesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

f18f5e37adeae1a21de3b966c9ed1b885d0642bb
s.iklenko <sergeyiklenko@gmail.com>
2021-08-19 19:35:07 +0300

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LibraryComponent } from './library/library.component';
+import { FriendsComponent } from './friends/friends.component';
+import { GamesComponent } from './games/games.component';
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CheckFormService } from './check-form.service'
+
+const appRoute: Routes =[
+  {path:'', component: GamesComponent},
+  {path:'library', component: LibraryComponent},
+  {path:'friends', component: FriendsComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'auth', component: AuthComponent},
+  {path:'reg', component: RegComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegComponent,
+    AuthComponent,
+    ProfileComponent,
+    LibraryComponent,
+    FriendsComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [CheckFormService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/reg/reg.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+// export interface regForm {
+//   name: string
+//   login: string
+//   email: string
+//   password: string | number
+// }
+
+@Component({
+  selector: 'app-reg',
+  templateUrl: './reg.component.html',
+  styleUrls: ['./reg.component.scss']
+})
+
+export class RegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
